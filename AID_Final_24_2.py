e7445a44d2698b36fb6467a9d8b4d8962d2252f5
BloomLonely <115198541+BloomLonely@users.noreply.github.com>
2024-12-20 21:35:29 +0900

    Update AID_Final_24_2.py

--- AID_Final_24_2.py
+++ AID_Final_24_2.py
@@ -394,10 +394,25 @@ if student_id:
         st.write(f"No data found for email: {student_id}")
         
 st.write(solution1)
-
 if student_id:
     data = get_student_data(student_id)
     if data is not None:
-        st.subheader("Student's Answer")
+        st.subheader("Question1. Student's Answer")
         student_answers = data[["1 - a", "1 - b", "1 - c", "1 - d", "1 - e"]]
         st.dataframe(student_answers, hide_index=True)
+
+st.write(solution2)
+if student_id:
+    data = get_student_data(student_id)
+    if data is not None:
+        st.subheader("Question2. Student's Answer")
+        student_answers = data[["2 - a", "2 - b", "2 - c", "2 - d"]]
+        st.dataframe(student_answers, hide_index=True)
+
+# st.write(solution3)
+# if student_id:
+#     data = get_student_data(student_id)
+#     if data is not None:
+#         st.subheader("Question3. Student's Answer")
+#         student_answers = data[["3 - a", "3 - b", "3 - c"]]
+#         st.dataframe(student_answers, hide_index=True)
